0ed323af2fd8f57842deedaf019a8ac407729d89
veera <veerababu.narni232@gmail.com>
2025-04-17 11:53:19 +0530

    day-3

---
+++ Day-3-tfvars/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "dev" {
+    ami = var.ami
+    instance_type = var.type
+  
+}
+
+# resource "aws_s3_bucket" "dev" {
+#     bucket = var.bucket
+  
+# }
+
+
+
+#terraform destroy -target=aws_s3_bucket.name if we want control particular resource use Target 
