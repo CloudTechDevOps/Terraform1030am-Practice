c4e96ea1c74514b9f38a97abe94ba93712d88e79
root <root@ip-172-31-81-243.ec2.internal>
2025-04-18 05:41:21 +0000

    day-4 dev2

--- Day-4-statefile-remote-config-s3/main.tf
+++ Day-4-statefile-remote-config-s3/main.tf
@@ -14,4 +14,11 @@ resource "aws_subnet" "test" {
     cidr_block = "10.0.1.0/24"
     vpc_id = aws_vpc.dev.id
   
-}
+
+  }
+  resource "aws_subnet" "prod" {
+    cidr_block = "10.0.2.0/24"
+    vpc_id = aws_vpc.dev.id
+
+
+  }
